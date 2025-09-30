4c4a8f3ad21632052fe07340d2ac1a1324ecf4c7
GiovanniDMCStrategicIT <giovanni@dmcstrategicit.com>
2025-09-30 15:23:09 -0500

    CN: Preprared the endpoint to update the data

--- routes/applicants.js
+++ routes/applicants.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addApplicant } = require('../controllers/applicantController');
+const { addApplicant, editApplicant } = require('../controllers/applicantController');
 
 router.post('/', addApplicant);
 
 router.get('/', (req, res) => {
     res.status(201).json({ message: 'Applicant is open...' });
 });
+
+router.post('/edit', editApplicant);
 
 module.exports = router;
